c523e5c73806316d470ffc003044074d97ba9f85
Saurabh Hindlekar <saurabh.hindlekar@hotmail.com>
2023-02-09 18:37:38 -0800

    scrape different website

--- data/topic_scraper.py
+++ data/topic_scraper.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 class QuoraScraper:
@@ -23,10 +25,13 @@ class QuoraScraper:
         self.driver.get(url)
 
     def scrape(self):
-        self.open_url('https://www.quora.com/topic/India')
-        elements = self.driver.find_elements(By.CLASS_NAME, 'js-focus-visible')
+        self.open_url('https://www.fluther.com/browse/meta/')
+        elements = self.driver.find_elements(By.CLASS_NAME, 'row2')
         print(str(len(elements)) + "!!!!!!!!!!!!!!!!!!")
+        for element in elements:
+            print(element.text)
         # time.sleep(100)
+
 
 
 if __name__ == "__main__":
